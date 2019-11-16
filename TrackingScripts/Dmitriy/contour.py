526b3f71ad878e7f371dbdc540a7f8841bf447fa
Robin Römer <rroemer@amt.rwth-aachen.de>
2019-11-16 23:31:37 +0100

    added a secure filter with tracking

--- TrackingScripts/Dmitriy/contour.py
+++ TrackingScripts/Dmitriy/contour.py
@@ -4,37 +4,47 @@ import cv2 as cv
 
 def get_contours(im):
 	imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-	ret, thresh = cv.threshold(imgray, 200, 255, 0)
+	imgray = cv.GaussianBlur(imgray,(5,5),0)
+	ret, thresh = cv.threshold(imgray, 190, 255, 0)
 	contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 	result = []
 	for cnt in contours:
-		rect = cv.minAreaRect(cnt)
-		#if (100 > cv.contourArea(cnt) > 6) and (rect[1][0] > 20 or rect[1][1] < 20) and (not(rect[0][0] < 30 or rect[0][1] >160 or rect[0][1]< 20)):
 		if cv.contourArea(cnt) > 1:
 			result.append(cnt)
 	return thresh, result
-		
+
 
 def get_first_center(im):
 	origin_y = 90
 	origin_x = 100
 	hresh, result = get_contours(im)
 	for cnt in contours:
-		M = cv.moments(cnt)
-		cx = int(M['m10']/M['m00'])
-		cy = int(M['m01']/M['m00'])
+		rect = cv.minAreaRect(cnt)
+		cx = rect[0][0]
+		cy = rect[0][1]
+		# M = cv.moments(cnt)
+		# cx = int(M['m10']/M['m00'])
+		# cy = int(M['m01']/M['m00'])
 
-def get_new_cors(rfx, rfy, contours):
+def get_new_cors(rfx, rfy, contours, searching):
 	dist = float("inf")
+	lostTrack = False
 	for cnt in contours:
-		M = cv.moments(cnt)
-		cx = int(M['m10']/M['m00'])
-		cy = int(M['m01']/M['m00'])
+		rect = cv.minAreaRect(cnt)
+		cx = rect[0][0]
+		cy = rect[0][1]
 		curr_dist = (cx-rfx)**2 + (cy-rfy)**2
 		if curr_dist < dist:
 			dist = curr_dist
-			result = (cx,cy)
-	return result
+			result = cnt
+			res_cx = rect[0][0]
+			res_cy = rect[0][1]
+	if dist > 10 and searching == False:
+		lostTrack = True
+		res_cx = rfx
+		res_cy = rfy
+	print(dist, res_cx, res_cy, lostTrack, searching)
+	return res_cx, res_cy, result, lostTrack
 
 # im = cv.imread('test.jpg')
 # contours = get_contours(im)
@@ -45,7 +55,7 @@ def get_new_cors(rfx, rfy, contours):
 # 	# cy = int(M['m01']/M['m00'])
 # 	# cv.circle(thresh, (cx,cy), 1, color=(0,255,255), thickness=2, lineType=8, shift=0)
 # 	# print(cx,cy)
-# 	# print(cv.contourArea(cnt))	
+# 	# print(cv.contourArea(cnt))
 # 	# cv.drawContours(thresh, [cnt], 0, (183,23,100), -1)
 # for row in thresh:
 # 	for i in range(len(row)):
--- TrackingScripts/Dmitriy/reader.py
+++ TrackingScripts/Dmitriy/reader.py
@@ -3,12 +3,12 @@ import numpy as np
 from contour import *
 
 backSub = cv.createBackgroundSubtractorMOG2()
-cap = cv.VideoCapture('/Users/home/Desktop/stuff/work/code/Junction/light-navigation/stick/output/output.avi')
+cap = cv.VideoCapture('output.avi')
 fps = 60
-frame_width = 110	
+frame_width = 110
 frame_height = 180
-out_writer = cv.VideoWriter('/Users/home/Desktop/stuff/work/code/Junction/light-navigation/stick/output/output_processed.avi',
-        cv.VideoWriter_fourcc('M','J','P','G'), 
+out_writer = cv.VideoWriter('output_processed.avi',
+        cv.VideoWriter_fourcc('M','J','P','G'),
         fps,
         (frame_width,frame_height))
 
@@ -16,24 +16,44 @@ i=0
 
 object_y = 90
 object_x = 100
-
+searching = True
+count = 0
 while(i < 20*60):
-	ret, new_frame = cap.read()
-	# fgMask = backSub.apply(new_frame)
-	imgray = cv.cvtColor(new_frame, cv.COLOR_BGR2GRAY)
-	# ret,thresh = cv.threshold(imgray, 200, 255, cv.THRESH_BINARY)
-	thresh, contours = get_contours(new_frame)
-	object_x, object_y = get_new_cors(object_x, object_y, contours)
-	for row in thresh:
-		for i in range(len(row)):
-			row[i] = 255
-	cv.circle(thresh, (object_x, object_y), 1, color=(0,255,255), thickness=2, lineType=8, shift=0)
-	cv.imshow('frame', thresh)
-	cv.waitKey(0)
+    ret, new_frame = cap.read()
+    # fgMask = backSub.apply(new_frame)
+    imgray = cv.cvtColor(new_frame, cv.COLOR_BGR2GRAY)
+    # ret,thresh = cv.threshold(imgray, 200, 255, cv.THRESH_BINARY)
+    thresh, contours = get_contours(new_frame)
+    object_x, object_y, final_cnt, lostTrack = get_new_cors(object_x, object_y, contours, searching)
+    for row in thresh:
+        for i in range(len(row)):
+            row[i] = 255
+    if lostTrack == False:
+        tmpCnt = final_cnt
+        cv.drawContours(thresh, [final_cnt], 0, (183,23,100), -1)
+        searching = False
+    elif searching == True:
+        tmpCnt = final_cnt
+        cv.drawContours(thresh, [final_cnt], 0, (183,23,100), -1)
+        seraching = False
+    elif count < 150:
+        #cv.drawContours(thresh, [final_cnt], 0, (250,23,2), -1)
+        cv.drawContours(thresh, [tmpCnt], 0, (183,23,100), -1)
+        count += 1
+    else:
+        count = 0
+        object_y = 90
+        object_x = 100
+        searching = True
+        print("reset")
 
-	out_writer.write(thresh)
+    #cv.circle(thresh, (object_x, object_y), 1, color=(0,255,255), thickness=2, lineType=8, shift=0)
+    cv.imshow('frame', thresh)
+    cv.waitKey(0)
+
+    out_writer.write(thresh)
 
 
-	i += 1
+    i += 1
 
 out_writer.release()
