809fd7f9b569dfc2768e75e5baf49d97b7583c48
SebastianWitt <sebastian.witt@rwth-aachen.de>
2019-11-17 02:05:41 +0200

    started scene switching

--- floorreact2/src/App.js
+++ floorreact2/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import FloorRenderer from './floorrenderer.js';
+import Home from './home';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.frenderer = <FloorRenderer render_floors={true}/>;
+        //FloorRenderer.changeState();
+    }
 
     render() {
+        //const element = <h1>Hello, world</h1>;
         return (
             <div className="App">
                 <header className="App-header">
-                    <FloorRenderer/>
+                    <FloorRenderer render_floors={false}/>
                 </header>
             </div>
-        );
+        )
+    }
+
+    switchScene() {
+        this.frenderer.changeState();
     }
 }
 
